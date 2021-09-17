b9fb3c501081bc6002edd758cc0510ae41ffb9e2
g3nty <soldatoved@gmail.com>
2021-09-17 23:35:28 +0500

    First commit

--- pr1/settings.py
+++ pr1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crm.apps.CrmConfig',  # доб. приложения
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'  #меняем язык
 
 TIME_ZONE = 'UTC'
 
