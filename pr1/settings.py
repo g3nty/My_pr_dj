d6a314a6ad66caa31facf3306f5824d9b2399c78
g3nty <soldatoved@gmail.com>
2021-09-28 15:12:54 +0500

    last-newpr2 commit

--- pr1/settings.py
+++ pr1/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'telembot.apps.TelembotConfig',
     'price.apps.PriceConfig',
     'cms.apps.CmsConfig',
     'crm.apps.CrmConfig',  # доб. приложения
